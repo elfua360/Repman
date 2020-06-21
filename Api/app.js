527228cb4507e6318e4dcc8c9cb6d7be676aa564
alexcote1 <ac@acbe.me>
2020-06-21 13:57:44 -0400

    fixed conflicting ports the right way

--- Api/app.js
+++ Api/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3001);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
