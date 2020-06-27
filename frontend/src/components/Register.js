8277fa617fdfc3023985496fa836e2265318a02f
William Presswood <willpreeswood@gmail.com>
2020-06-27 14:51:19 -0400

    added components folder

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Register
+{
+  const doRegister= async event=>
+  {
+    event.preventDefault();
+  };
+  return{
+    <h1 id="registerTitle">Register Here</h1>
+    <div id="register">
+    <form onSubmit=(doRegister)>
+    <span id="usernameRegisterTitle">Username</span>
+    <input type="text" id="usernameRegister" placeholder="Username">
+    <br>
+    <input type="password" id="passwordCreate" placeholder="Password">
+    <br>
+    <input type="submit" id="createButton" class="button" value="Create Account"
+    onClick=(doRegister)>
+    </form>
+    <span id="registerResult"></span>
+    </div>
+
+  };
+};
+export default Register;
