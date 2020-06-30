127da2789bdee2b525da2db93bfe41be563e454e
NAME <nayoshi12@gmail.com>
2020-06-30 05:40:35 -0400

    Added bootstrap and react-bootstrap for easy front end design

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 function Login()
 {
   const doLogin= async event=>
@@ -8,20 +9,41 @@ function Login()
   };
   return(
   <>
-    <h1 id="loginTitle">Login Here</h1>
-    <div id="login">
-    <form onSubmit={doLogin}>
-    <span id="usernameLoginTitle">Username</span>
-    <input type="text" id="usernameLogin" placeholder="Username"/>
-    <br/>
-    <span id="passwordLoginTitle">Password</span>
-    <input type="password" id="passwordEnter" placeholder="Password"/>
-    <br/>
-    <input type="submit" id="loginButton" class="button" value="Sign In"
-    onClick={doLogin}/>
-    </form>
-    <span id="loginResult"></span>
-    </div>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    {/*<h1 id="loginTitle">Login Here</h1>*/}
+    {/*<div id="login">*/}
+    {/*<form onSubmit={doLogin}>*/}
+    {/*<span id="usernameLoginTitle">Username</span>*/}
+    {/*<input type="text" id="usernameLogin" placeholder="Username"/>*/}
+    {/*<br/>*/}
+    {/*<span id="passwordLoginTitle">Password</span>*/}
+    {/*<input type="password" id="passwordEnter" placeholder="Password"/>*/}
+    {/*<br/>*/}
+    {/*<input type="submit" id="loginButton" class="button" value="Sign In"*/}
+    {/*onClick={doLogin}/>*/}
+    {/*</form>*/}
+    {/*<span id="loginResult"></span>*/}
+    {/*</div>*/}
+
     </>
 
   );
