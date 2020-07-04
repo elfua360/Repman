d28529fc102e4258d85b8066205109b3c125e5b8
NAME <nayoshi12gaming@gmail.com>
2020-07-04 04:42:19 -0400

    Main App will rerender when the button clicks -Matt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,35 +2,52 @@ import React from 'react';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import Nav from "react-bootstrap/Nav";
+import RegisterPage from "./pages/RegisterPage";
 
-function App() {
-    return (
-        <>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: "login"
+        }
+    }
+
+    test = () => {
+        console.log(this.state.page)
+        if(this.state.page === "login"){
+            return <LoginPage/>;
+        }else{
+            return <RegisterPage/>;
+        }
+    };
+    render() {
+        return (
             <div className="Header">
                 <h1 className="header">Repman</h1>
                 <br/>
                 <Nav
                     variant="tabs"
                     onSelect={(selectedKey) => {
-                        alert(selectedKey)
+                        this.setState(()=>{
+                            return {page: selectedKey};
+                        })
                     }}
                     className="navigation"
-                    defaultActiveKey="link-1"
+                    defaultActiveKey="login"
                 >
                     <Nav.Item>
-                        <Nav.Link eventKey="link-1">Login</Nav.Link>
+                        <Nav.Link eventKey="login">Login</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-2">Register</Nav.Link>
+                        <Nav.Link eventKey="register">Register</Nav.Link>
                     </Nav.Item>
                 </Nav>
                 <br/>
+                {this.test()}
 
-                <LoginPage/>
-            </div>
-
-        </>
-    );
+            </div>)
+    }
 }
+
 
 export default App;
