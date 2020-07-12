1caaaf1c2f994b8b2549e2aeb8be3e91219c4d28
Alex <ad.23970@hotmail.com>
2020-07-11 20:22:46 -0400

    update register tab appearance to match login

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Register() {
     const doRegister = async event => {
-        event.preventDefault();
+        var firstName = document.getElementById("firstNameText").value;
+        var lastName = document.getElementById("lastNameText").value;
+        var email = document.getElementById("emailText").value;
+        var password = document.getElementById("passwordText").value;
     };
     return (
+        <>
+            <Form onSubmit={Register}>
+                <Form.Group controlId="formFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter first name"/>
+                </Form.Group>
+                <Form.Group controlId="formLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter last name"/>
+                </Form.Group>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"/>
+                </Form.Group>
+
+
+
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+        </>
+
+
+
+        /*
         <>
             <h1 id="registerTitle">Register Here</h1>
             <div id="register">
@@ -21,6 +59,7 @@ function Register() {
                 <span id="registerResult"></span>
             </div>
         </>
+        */
 
     );
 };
