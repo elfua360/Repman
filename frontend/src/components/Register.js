f973f5928cfaf619c6cbf6d9d0b7d99e4227a8f1
Alex <ad.23970@hotmail.com>
2020-07-11 21:24:39 -0400

    almost connect register endpoint

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,39 +4,64 @@ import Button from 'react-bootstrap/Button'
 
 function Register() {
     const doRegister = async event => {
-        var firstName = document.getElementById("firstNameText").value;
-        var lastName = document.getElementById("lastNameText").value;
-        var email = document.getElementById("emailText").value;
-        var password = document.getElementById("passwordText").value;
+        var firstName = document.getElementById("registerFirstName").value;
+        var lastName = document.getElementById("registerLastName").value;
+        var email = document.getElementById("registerEmail").value;
+        var password = document.getElementById("registerPassword").value;
+
+        var jsonPayload = JSON.stringify({"first_name": firstName, "last_name": lastName, "email": email, "password": password});
+        alert(jsonPayload); // DELETEME
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+
+        try{
+            //xhr.addEventListener
+        }
+        catch(err){
+            /*class="alert alert-danger" role="alert"*/
+            document.getElementById("registerError").className = "alert alert-danger";
+            document.getElementById("registerError").innerHTML = "Something went wrong!"; /*err.message*/
+        
+        }
+
+
+        event.preventDefault();
     };
     return (
         <>
-            <Form onSubmit={Register}>
-                <Form.Group controlId="formFirstName">
+            <Form onSubmit={doRegister}>
+                <Form.Group controlId="registerFirstName">
                     <Form.Label>First Name</Form.Label>
                     <Form.Control type="text" placeholder="Enter first name"/>
                 </Form.Group>
-                <Form.Group controlId="formLastName">
+
+                <Form.Group controlId="registerLastName">
                     <Form.Label>Last Name</Form.Label>
                     <Form.Control type="text" placeholder="Enter last name"/>
                 </Form.Group>
-                <Form.Group controlId="formEmail">
+
+                <Form.Group controlId="registerEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email"/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone.
                     </Form.Text>
                 </Form.Group>
-                <Form.Group controlId="formBasicPassword">
+
+                <Form.Group controlId="registerPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password"/>
                 </Form.Group>
 
-
-
                 <Button variant="primary" type="submit">
                     Sign Up
                 </Button>
+
+                
+                <div role="alert" id="registerError">
+                </div>
             </Form>
         </>
 
