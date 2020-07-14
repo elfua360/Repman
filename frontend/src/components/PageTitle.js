d87f7d6ca5da6f88328e294dbd8f1afb18872a84
William Presswood <willpresswood@gmail.com>
2020-07-14 15:45:32 -0400

    forgot to save changes, re commit

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 function PageTitle
 {
-  return
-  {
-    <h1 id="topTitleBanner">Recipe App Name TBD</h1>
+  return(
+    <h1 id="topTitleBanner">Repman</h1>
     
-  };
+  );
 };
 export default PageTitle;
