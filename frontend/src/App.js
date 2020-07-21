c4882fe56849f5b3aa72fa258a97b6cb09ae31a4
William Presswood <willpresswood@gmail.com>
2020-07-21 16:47:44 -0400

    Changed state to isLogged in/added MainPage to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import MainPage from './pages/MainPage'
+import PageTitle from './components/PageTitle'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: "login"
+            isLoggedIn: false
         }
     }
 
     router = () => {
-        if (this.state.page === "login") {
-            return <LandingPage/>;
+        if(this.state.isLoggedIn){
+            return (
+            <>
+                <PageTitle/>
+                <MainPage/>
+             </>
+             )
+		}
+        else{
+            return (
+            <>
+                <PageTitle/>
+                <LandingPage/>
+            </>
+            )
         }
-    };
+    }
 
     render() {
         return (
