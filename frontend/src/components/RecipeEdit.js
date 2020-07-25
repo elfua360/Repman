9c2749c98b0e9093193996309b206c17a7ddceb4
William Presswood <willpresswood@gmail.com>
2020-07-25 06:41:26 -0400

    updated for tags fix

--- frontend/src/components/RecipeEdit.js
+++ frontend/src/components/RecipeEdit.js
@@ -4,11 +4,11 @@ import {Modal,Form,Button} from 'react-bootstrap';
 class RecipeEdit extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: "", ingredients: "", steps: ""};
-    this.handleRecipeNameChange = this.handleRecipeNameChange.bind(this);
-    this.handleRecipeIngredientsChange = this.handleRecipeIngredientsChange.bind(this);
-    this.handleRecipeStepsChange = this.handleRecipeStepsChange.bind(this);
-    this.handleRecipeTagsChange = this.handleRecipeTagsChange.bind(this);
+    this.state = {name: "", ingredients: "", steps: "", tags: ""};
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleIngredientsChange = this.handleIngredientsChange.bind(this);
+    this.handleStepsChange = this.handleStepsChange.bind(this);
+    this.handleTagsChange = this.handleTagsChange.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
   }
@@ -16,25 +16,28 @@ class RecipeEdit extends React.Component {
     const prevName = state.prevName;
     const prevIngredients = state.prevIngredients;
     const prevSteps = state.prevSteps;
+    const prevTags = state.prevTags;
     const name = prevName !== props.recipe.name ? props.recipe.name : state.name;
     const ingredients = prevIngredients !== props.recipe.ingredients.join(",") ? props.recipe.ingredients.join(",") : state.ingredients;
     const steps = prevSteps !== props.recipe.steps.join(",") ? props.recipe.steps.join(",") : state.steps;
+    const tags = prevTags !== props.recipe.tags.join(",") ? props.recipe.tags.join(",") : state.tags;
     return {
       prevName: props.recipe.name, name,
       prevIngredients: props.recipe.ingredients.join(","), ingredients,
       prevSteps: props.recipe.steps.join(","), steps,
+      prevTags: props.recipe.tags.join(","), tags,
     }
   }
-  handleRecipeNameChange(e) {
+  handleNameChange(e) {
     this.setState({name: e.target.value});
   }
-  handleRecipeIngredientsChange(e) {
+  handleIngredientsChange(e) {
     this.setState({ingredients: e.target.value});
   }
-  handleRecipeStepsChange(e) {
+  handleStepsChange(e) {
     this.setState({steps: e.target.value});
   }
-  handleRecipeTagsChange(e) {
+  handleTagsChange(e) {
     this.setState({tags: e.target.value});
   }
   handleEdit(e) {
@@ -51,7 +54,7 @@ class RecipeEdit extends React.Component {
   handleCancel() {
     const onEditModal = this.props.onEditModal;
     this.setState({name: this.props.recipe.name, ingredients: this.props.recipe.ingredients.join(","),
-    steps: this.props.recipe.steps.join(",")});
+    steps: this.props.recipe.steps.join(","), tags: this.props.recipe.tags.join(",")});
     onEditModal();
   }
   render() {
@@ -71,19 +74,19 @@ class RecipeEdit extends React.Component {
         <Modal.Body>
           <Form.Group controlId="recipeName">
             <Form.Label>Recipe Name</Form.Label>
-            <Form.Control type="text" required onChange={this.handleRecipeNameChange} value={this.state.name} placeholder="Enter Recipe Name" />
+            <Form.Control type="text" required onChange={this.handleNameChange} value={this.state.name} placeholder="Enter Recipe Name" />
           </Form.Group>
           <Form.Group controlId="recipeIngredients">
             <Form.Label>Recipe Ingredients</Form.Label>
-            <Form.Control as="textarea" type="text" rows="3" required onChange={this.handleRecipeIngredientsChange} value={this.state.ingredients} placeholder="separate by commas" />
+            <Form.Control as="textarea" type="text" rows="3" required onChange={this.handleIngredientsChange} value={this.state.ingredients} placeholder="separate by commas" />
           </Form.Group>
           <Form.Group controlId="recipeSteps">
             <Form.Label>Recipe Steps</Form.Label>
-            <Form.Control as="textarea" type="text" rows="3" required onChange={this.handleRecipeStepsChange} value={this.state.steps} placeholder="separate by commas"/>
+            <Form.Control as="textarea" type="text" rows="3" required onChange={this.handleStepsChange} value={this.state.steps} placeholder="separate by commas"/>
           </Form.Group>
           <Form.Group controlId="tags">
             <Form.Label>Tags</Form.Label>
-            <Form.Control as="textarea" type="text" rows="1" required onChange={this.handleRecipeTagsChange}
+            <Form.Control as="textarea" type="text" rows="1" required onChange={this.handleTagsChange}
                           value={this.state.tags} placeholder="separate by commas"/>
           </Form.Group>
         </Modal.Body>
